08411d6526b29ffecc2b4bf04880650f64cfa113
Noritaka Kamiya <noritakakamiya@gmail.com>
2020-11-29 08:20:48 +0900

    Init Project

---
+++ Raiden/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Raiden
+//
+//  Created by nori on 2020/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
